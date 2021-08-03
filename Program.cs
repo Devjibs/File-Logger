b129fac7afad3946962672dd2ba3fc6567da2205
JiboGithub <ajidejibola@yahoo.com>
2021-08-03 22:09:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                path: "c:\\mylogger\\logs\\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Application");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+
+                Log.Fatal(e, "Unable to start Application");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
